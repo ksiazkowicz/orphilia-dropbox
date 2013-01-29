99c8e0cff9d33fd2727b1ae1d2b2211b7c7a03a4
pisarzk@gmail.com <pisarzk@gmail.com>
2013-01-29 10:38:23 +0100

    remove old and not working build.bat
    Win32 will be eventually supported by the build.py script. This one is
    obsolete.

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-del orphilia.py
-type parser.py >> orphilia.py
-type header.py >> orphilia.py
-type api\client.py >> orphilia.py
-type api\rest.py >> orphilia.py
-type api\session.py >> orphilia.py
-type main.py >> orphilia.py
-echo Build scripts parsed.
